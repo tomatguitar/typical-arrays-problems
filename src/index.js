f66896d8208a93ed2774475a2ff3a3b81440b5f2
tomatguitar <zkarman@mail.ru>
2020-09-30 17:43:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    return array.reduce((a, b) => a + b) / array.length
+  } else {
+    return 0;
+  }
 }
